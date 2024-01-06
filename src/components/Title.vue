2ce78bea0e260b8bf5c002380f10051061fc27bb
Pablo <jhoelpablo02@gmail.com>
2024-01-06 05:04:55 -0500

    add title component

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,24 +1,30 @@
 
 <template>
-    <div class="text-5xl font-extrabold text-text_orange">
-      {{ title }}
-    </div>
-  </template>
+  <div class="border-t-2 border-personal-ternary">
+
+  </div>
+  <div class="text-5xl font-extrabold text-white title">
+    {{ title }}
+  </div>
+</template>
   
-  <script lang="ts">
-  import { defineComponent, PropType } from 'vue';
-  
-  export default defineComponent({
-    props: {
-      title: {
-        type: String as PropType<string>,
-        required: true,
-      },
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String as PropType<string>,
+      required: true,
     },
-  });
-  </script>
+  },
+});
+</script>
   
-  <style scoped>
-  /* Estilos opcionales para tu componente */
-  </style>
+<style scoped>
+/* Estilos opcionales para tu componente */
+.title {
+  text-shadow: -1px 0 yellowgreen, 0 1px yellowgreen, 2.5px 0 yellowgreen, 0 -1px yellowgreen;
+}
+</style>
   
