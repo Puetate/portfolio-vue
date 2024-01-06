77e476cfba0e0d5c39c49ba52e103f4948d86c36
Pablo <jhoelpablo02@gmail.com>
2024-01-06 05:05:42 -0500

    add section contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,14 +1,69 @@
 <script setup lang="ts">
+import { AutoTyperVue } from 'auto-typer-vue3';
 import Title from './Title.vue';
 
-const title = "CONTACTO";
+const title = "CONTACT";
+let text = [
+    'Get in Touch',
+    'Send me a message!'
+];
 
 </script>
 
 <template>
-
-<section class="h-screen relative flex flex-col justify-center gap-8 text-text_withe" id="contact">
+    <section class="h-screen flex flex-col justify-center gap-8 text-teal-300" id="contact">
         <Title :title=title></Title>
-    </section>
 
+        <div class="">
+            <AutoTyperVue class="font-semibold text-xl text-orange-300" componentTag="h1" :text="text" :typingDelay=150
+                :deletingDelay=100 :startDelay=600 :repeat="true" :waitBeforeDeleteDelay=200 />
+        </div>
+        <div class="w-2/3 text-justify text-personal-white">
+            Connect with me to discuss exciting opportunities, collaborations, and projects. I can be reached via <a
+                class="underline underline-offset-1 text-slate-300" rel="stylesheet"
+                href="mailto:jhoelpablo02@gmail.com">jhoelpablo02@gmail.com</a>. and my <a
+                class="underline underline-offset-1 text-slate-300" rel="stylesheet"
+                href="https://www.linkedin.com/in/jhoel-puetate-915056127/">LinkedIn</a>. Looking forward to hearing from
+            you and
+            exploring new possibilities together
+        </div>
+        <div class="flex flex-col gap-6 w-2/3">
+            <div class="flex flex-row justify-between gap-8">
+                <div class="w-1/2">
+                    <label for="nombre" class="block text-gray-300 text-sm font-bold mb-2">Name:</label>
+                    <input type="text" id="nombre" name="nombre"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none focus:shadow-outline-blue placeholder-gray-300 focus:border-blue-300 bg-personal-ternary"
+                        placeholder="Your Name">
+                </div>
+                <div class="w-1/2">
+
+                    <label for="correo" class="block text-gray-300 text-sm font-bold mb-2">Email:</label>
+                    <input type="email" id="correo" name="correo"
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none focus:shadow-outline-blue placeholder-gray-300 focus:border-blue-300 bg-personal-ternary"
+                        placeholder="Your Email">
+                </div>
+            </div>
+            <div>
+                <label for="asunto" class="block text-gray-300 text-sm font-bold mb-2">Message:</label>
+                <input type="text" id="asunto" name="asunto"
+                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none focus:shadow-outline-blue placeholder-gray-300 focus:border-blue-300 bg-personal-ternary"
+                    placeholder="Your Message">
+
+            </div>
+            <button class="w-full p-1.5 rounded-md font-medium bg-text_orange border-spacing-1 text-black"
+                style="background: linear-gradient(to right, #6dc1f5, #0835b3);">SEND</button>
+
+        </div>
+
+        <div class="">
+
+            <div class="mb-4">
+
+            </div>
+        </div>
+    </section>
 </template>
+
+<style scoped>
+@import "../../node_modules/auto-typer-vue3/dist/style.css";
+</style>
