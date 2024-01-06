5f18c06af5e3d490579dc97d7c3c43aaa9173a02
Pablo <jhoelpablo02@gmail.com>
2024-01-06 05:06:29 -0500

    add section skill

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,68 +1,49 @@
 <script setup lang="ts">
-import { IconBrandLinkedin, IconDatabase } from '@tabler/icons-vue';
 import SkillViewVue, { Skill } from './SkillView.vue';
 import Title from './Title.vue';
-import { IconBrandJavascript } from '@tabler/icons-vue';
-import { IconBrandTypescript } from '@tabler/icons-vue';
-import { IconBrandPhp } from '@tabler/icons-vue';
-import { IconBrandReact } from '@tabler/icons-vue';
-import { IconBrandNodejs } from '@tabler/icons-vue';
-import { IconBrandLaravel } from '@tabler/icons-vue';
-import { IconBrandAngular } from '@tabler/icons-vue';
-import { IconBrandVue } from '@tabler/icons-vue';
-import { IconBrandMongodb } from '@tabler/icons-vue';
-import { IconBrandMysql } from '@tabler/icons-vue';
-import { IconBrandFlutter } from '@tabler/icons-vue';
-import { IconBrandPython } from '@tabler/icons-vue';
-import { IconBrandGit } from '@tabler/icons-vue';
-import { IconServer } from '@tabler/icons-vue';
-import { IconCodeCircle2 } from '@tabler/icons-vue';
-import { IconSquareRotated } from '@tabler/icons-vue';
 
 const title = "SKILLS";
 
 const titleLanguage = "LENGUAJES DE PROGRAMACIÓN";
 const skillLanguages: Skill[] = [
-    { nameSkill: "DART", icon: IconCodeCircle2, color: "#26a5ff" },
-    { nameSkill: "JAVASCRIPT", icon: IconBrandJavascript, color: "yellow" },
-    { nameSkill: "TYPESCRIPT", icon: IconBrandTypescript, color: "#0077d8" },
-    { nameSkill: "JAVA", icon: IconBrandLinkedin, color: "blue" },
-    { nameSkill: "PHP", icon: IconBrandPhp, color: "cyan" },
-    { nameSkill: "PYTHON", icon: IconBrandPython, color: "yellow" },
+    { nameSkill: "PHP", icon: "php", color: "cyan" , level:"beginner"},
+    { nameSkill: "PYTHON", icon: "python", color: "yellow" , level:"beginner"},
+    { nameSkill: "JAVA", icon: "java", color: "blue" , level:"intermediate"},
+    { nameSkill: "JAVASCRIPT", icon: "javascript", color: "yellow" , level:"intermediate"},
+    { nameSkill: "TYPESCRIPT", icon: "typescript", color: "#0077d8" , level:"intermediate"},
+    { nameSkill: "DART", icon: "dart", color: "#26a5ff" , level:"advanced"},
 ];
 
 const titleFramework = "FRAMEWORKS";
 const skillFrameworks: Skill[] = [
-    { nameSkill: "FLUTTER", icon: IconBrandFlutter, color: "#12c4ff" },
-    { nameSkill: "REACT", icon: IconBrandReact, color: "red" },
-    { nameSkill: "NESTJS", icon: IconBrandNodejs, color: "blue" },
-    { nameSkill: "LARAVEL", icon: IconBrandLaravel, color: "red" },
-    { nameSkill: "ANGULAR", icon: IconBrandAngular, color: "red" },
-    { nameSkill: "VUE", icon: IconBrandVue, color: "green" },
-
+    { nameSkill: "LARAVEL", icon: "laravel", color: "red" , level:"beginner"},
+    { nameSkill: "ANGULAR", icon: "angular", color: "red" , level:"beginner"},
+    { nameSkill: "VUE", icon: "vue", color: "green" , level:"beginner"},
+    { nameSkill: "NESTJS", icon: "nest", color: "blue" , level:"intermediate"},
+    { nameSkill: "REACT", icon: "react", color: "red" , level:"intermediate"},
+    { nameSkill: "FLUTTER", icon: "flutter", color: "#12c4ff" , level:"advanced"},
 ];
 
 const titleDatabase = "BASE DE DATOS";
 const skillDatabases: Skill[] = [
-    { nameSkill: "MONGOBD", icon: IconBrandMongodb, color: "green" },
-    { nameSkill: "MYSQL", icon: IconBrandMysql, color: "#0096d1" },
-    { nameSkill: "SQL SERVER", icon: IconDatabase, color: "#1DA1F2" },
-    { nameSkill: "POSTGRESQL", icon: IconServer, color: "blue" },
-
+    { nameSkill: "POSTGRESQL", icon: "postgresql", color: "blue" , level:"beginner"},
+    { nameSkill: "SQL SERVER", icon: "sqlserver", color: "#1DA1F2" , level:"intermediate"},
+    { nameSkill: "MONGOBD", icon: "mongo", color: "green" , level:"intermediate"},
+    { nameSkill: "MYSQL", icon: "mysql", color: "#0096d1" , level:"advanced"},
 ];
 
 const titleTool = "SOFTWARE Y HERRAMIENTAS";
 const skillTools: Skill[] = [
-    // { nameSkill: "a", icon: "", color:"blue" },
-    { nameSkill: "GIT", icon: IconBrandGit, color: "blue" },
-    { nameSkill: "JIRA", icon: IconSquareRotated, color: "blue" },
-
+    // { nameSkill: "a", icon: "", color:"blue" , level:"beginner"},
+    { nameSkill: "GIT", icon: "git", color: "blue" , level:"intermediate"},
+    { nameSkill: "JIRA", icon: "jira", color: "blue" , level:"intermediate"},
 ];
+
 
 </script>
 
 <template>
-    <section class="h-screen relative flex flex-col justify-center gap-8 text-text_withe" id="skills">
+    <section class="relative flex flex-col justify-center gap-8 text-text_withe" id="skills">
         <Title :title=title></Title>
         <!-- <div class="text-5xl font-extrabold  text-text_withe">
             {{ title }}
