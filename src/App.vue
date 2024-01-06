f13bf15cc5a3885345f6bad0f3fda24f73133450
Pablo <jhoelpablo02@gmail.com>
2024-01-06 05:09:13 -0500

    change styles app

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import About from './components/About.vue';
 import Contact from './components/Contact.vue';
 import Experiences from './components/Experiences.vue';
 import Home from './components/Home.vue';
+import Leave from './components/Leave.vue';
 import Projects from './components/Projects.vue';
 import SideBar from './components/SideBar.vue';
 import Skills from './components/Skills.vue';
@@ -11,15 +12,16 @@ import Skills from './components/Skills.vue';
 </script>
 
 <template>
-  <main class="flex h-screen">
-    <SideBar class="w-1/6 bg-bg_main text-white p-4 flex flex-col justify-center items-center" />
-    <div class="w-5/6  bg-gray-800 content overflow-y-auto max-h-screen pl-14" id="content">
+  <SideBar class="" />
+  <main class="text-personal-white flex h-full flex-col overflow-y-auto overflow-x-hidden scroll-smooth">
+    <div class="px-24" id="content">
       <Home />
       <About />
       <Skills />
       <Projects />
       <Experiences />
       <Contact />
+      <Leave/>
     </div>
   </main>
 </template>
