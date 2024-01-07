f7a2c38bfc93c612cf15982e201f0430159cdf6a
Pablo <jhoelpablo02@gmail.com>
2024-01-07 18:50:38 -0500

    add assets projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -8,7 +8,7 @@ const projects: Project[] = [
                 nameProject: "Vivo Vivo",
                 descProject: "Vivo Vivo enables you to send alerts to your family group and share your real-time location in emergency situations, ensuring swift communication and enhanced safety measures.",
                 linkGit: "",
-                pictureProject: "/src/assets/p1.png",
+                pictureProject: ["../assets/pr_vivovivo.webp"],
                 technologies: [
                         { name: "Flutter", icon: "flutter", color: "blue" },
                         { name: "NestJS", icon: "nest", color: "red" },
@@ -22,7 +22,7 @@ const projects: Project[] = [
                 nameProject: "Capy Bills",
                 descProject: "Electronic invoicing system with solid functionalities to manage suppliers, inventory, users, purchases and sales. This comprehensive platform empowers businesses by providing efficient control and oversight across key aspects of operations, ensuring a simplified and organized approach to billing and financial management.",
                 linkGit: "",
-                pictureProject: "/src/assets/p1.png",
+                pictureProject: ["../assets/pr_capy_1.webp"],
                 technologies: [
                         { name: "React", icon: "React", color: "cyan" },
                         { name: "Spring", icon: "spring", color: "green" },
@@ -34,7 +34,7 @@ const projects: Project[] = [
                 nameProject: "Arte CCT",
                 descProject: "Virtual gallery that shows the art collection of the Tungurahua Culture House. Immerse yourself in the rich history of each painting, accompanied by narrations that provide insight into the artistic journey. Using the QR scanner feature you can easily discover specific works of art, enhancing your overall gallery experience.",
                 linkGit: "",
-                pictureProject: "/src/assets/p1.png",
+                pictureProject: ["../assets/pr_artecct.webp"],
                 technologies: [
                         { name: "Flutter", icon: "flutter", color: "blue" },
                         { name: "Spring", icon: "spring", color: "green" },
