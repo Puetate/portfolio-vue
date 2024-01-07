eefe4ba4a3d3a89e81b1907bf590eb6f4047d4f5
Pablo <jhoelpablo02@gmail.com>
2024-01-07 18:50:16 -0500

    add animation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,35 +1,43 @@
 <script setup lang="ts">
 import { AutoTyperVue } from "auto-typer-vue3";
 import SocialNetworks from "./SocialNetworks.vue";
+import { Vue3Lottie } from 'vue3-lottie'
+
+import AnimDev from '../assets/an_dev_1.json'
 let text = [
     'Welcome to my portfolio as a developer.',
 ];
 </script>
 
 <template>
-    <section class="h-screen relative flex flex-col justify-center gap-8 text-text_withe" id="home">
-        <div class="">
-            <p class="font-bold text-2xl">
-                I'm Jhoel Puetate a
-            </p>
-            <div class="h-auto w-1/3 text-7xl font-extrabold">
-                <span class="text-white " id="name">FULL STACK SOFTWARE DEVELOPER </span>
+    <section class="w-full my-9 flex flex-row justify-around  items-center text-text_withe" id="home">
+        <div class="flex flex-col justify-start gap-8 mt-6">
+            <div class="">
+                <p class="font-bold text-2xl">
+                    I'm Jhoel Puetate a
+                </p>
+                <div class="h-auto w-2/3 text-7xl font-extrabold">
+                    <span class="text-white " id="name">FULL STACK SOFTWARE DEVELOPER </span>
+                </div>
+                <!-- <span class="text-personal-white" id="surname">PUETATE</span> -->
             </div>
-            <!-- <span class="text-personal-white" id="surname">PUETATE</span> -->
-        </div>
-        <div class="text-personal-white">
-            <AutoTyperVue class="font-semibold text-xl" componentTag="h1" :text="text" :typingDelay=50 :deletingDelay=75
-                :waitBeforeDeleteDelay=200 />
-        </div>
+            <div class="text-personal-white">
+                <AutoTyperVue class="font-semibold text-xl" componentTag="h1" :text="text" :typingDelay=50 :deletingDelay=75
+                    :waitBeforeDeleteDelay=200 />
+            </div>
 
-        <div class="text-lg text-personal-white">
-            If you wish to get in touch with me, write to me at:
-            <a class="underline underline-offset-1 text-slate-300" rel="stylesheet"
-                href="mailto:jhoelpablo02@gmail.com">jhoelpablo02@gmail.com</a>.
+            <div class="text-lg text-personal-white">
+                If you wish to get in touch with me, write to me at:
+                <a class="underline underline-offset-1 text-slate-300" rel="stylesheet"
+                    href="mailto:jhoelpablo02@gmail.com">jhoelpablo02@gmail.com</a>.
+            </div>
+            <button class="w-36 p-1.5 rounded-md border-spacing-1 text-white"
+                style="background: linear-gradient(to right, #6dc1f5, #0835b3);">Download CV</button>
+            <SocialNetworks />
         </div>
-        <button class="w-36 p-1.5 rounded-md border-spacing-1 text-white"
-            style="background: linear-gradient(to right, #6dc1f5, #0835b3);">Descarga mi CV</button>
-        <SocialNetworks />
+        <div class="flex-1">
+            <Vue3Lottie :animationData="AnimDev" :width="400" />
+        </div>
     </section>
 </template>
 
