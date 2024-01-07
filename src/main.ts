58229ae162e863e9fc540c8d6b6ba1b2b28cb010
Pablo <jhoelpablo02@gmail.com>
2024-01-07 18:54:31 -0500

    add vue3Lottie

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import './style.css'
-const app = createApp(App);
+import Vue3Lottie  from 'vue3-lottie';
+const app = createApp(App).use(Vue3Lottie);
 
 app.mount('#app');
