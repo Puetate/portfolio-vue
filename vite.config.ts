d43d0552341dec53b461df2c2a84b469a2506935
Pablo <jhoelpablo02@gmail.com>
2024-01-07 19:17:20 -0500

    add assets in dist

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
       vue(),
   ],
+  build: { chunkSizeWarningLimit: 1600, },
   resolve: {
       alias: {
           '@': fileURLToPath(new URL('./src', import.meta.url))
